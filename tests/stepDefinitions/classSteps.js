23887b54f5e2fb8dc5596a38689fed54f7d76450
mathumathi0989 <mathumathi.b@gmail.com>
2025-03-18 16:20:24 -0400

    added few class feature scenarios

--- tests/stepDefinitions/classSteps.js
+++ tests/stepDefinitions/classSteps.js
@@ -27,17 +27,35 @@ Given('Admin is on the home page after login', async function ({loggedInPage}){
   });
 
 
-  // When('Admin enters {string} and {string} in the login form', async ({}, username, password) => {
-  //   const testData = getTestData(); // Get data from Excel
-  //   for (let data of testData) {
-  //     console.log(`Attempting login with username: ${data.Username} and password: ${data.Password}`);
-  //   }
-  // });
+  Then('Admin should see the Search Bar in Manage class page', async ({}) => {
+    // Step: Then Admin should see the Search Bar in Manage class page
+    // From: tests/features/04_class.feature:18:1
+  });
   
-  // Then('Admin should verify for data driven', async ({}) => {
-  //  console.log("Data driven test completed");
-  // });
-
+  Then('Admin should see the datatable heading like Batchname,class topic,class description,status,class Date,staff name,Edit\\/Delete', async ({}) => {
+    // Step: Then Admin should see the datatable heading like Batchname,class topic,class description,status,class Date,staff name,Edit/Delete
+    // From: tests/features/04_class.feature:22:1
+  });
+  
+  Then('Admin should see the {string} and enabled pagination controls under the data table', async ({}, arg) => {
+    // Step: Then Admin should see the " showing x to y of  z entries" and enabled pagination controls under the data table
+    // From: tests/features/04_class.feature:26:1
+  });
+  
+  Then('Admin should see the Sort icon of all the field in the datatable.', async ({}) => {
+    // Step: Then Admin should see the Sort icon of all the field in the datatable.
+    // From: tests/features/04_class.feature:30:1
+  });
+  
+  Then('Admin should see the Delete button under the Manage class page header.', async ({}) => {
+    // Step: Then Admin should see the Delete button under the Manage class page header.
+    // From: tests/features/04_class.feature:34:1
+  });
+  
+  Then('Admin should see Total no of classes in below of the data table.', async ({}) => {
+    // Step: Then Admin should see Total no of classes in below of the data table.
+    // From: tests/features/04_class.feature:38:1
+  });
 
 
 
