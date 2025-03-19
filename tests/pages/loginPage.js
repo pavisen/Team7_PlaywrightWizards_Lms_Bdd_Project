15e21cf9082603805f9bc5ff574e31d24239c534
AnusuyaSDET169 <anusuyas.official@gmail.com>
2025-03-18 22:47:40 -0500

    Login scenarios done

--- tests/pages/loginPage.js
+++ tests/pages/loginPage.js
@@ -11,6 +11,11 @@ class LoginPage {
     this.errorMessage = this.page.locator('text=Invalid username and password'); 
     this.emptyUsernameErrorLocator = this.page.getByText('Please enter your user name') 
     this.emptyPasswordErrorLocator = this.page.getByText('Please enter your password') 
+    this.passwordFieldLabel = page.locator('//*[text()="Password"]');
+    this.usernameFieldLabel = page.locator('//*[text()="User"]');
+    this.asterisk = page.locator('xpath=//*[@id="mat-form-field-label-1"]/span[2]');
+    //this.asterisk = page.locator('//span[contains(text(),"*")]');
+
   }
 
   async navigateToLoginPage(url) {
@@ -40,6 +45,32 @@ class LoginPage {
   async getEmptyPasswordError() {
     return this.emptyPasswordErrorLocator;
   }
+  async isAsteriskPresent() {
+    return await this.asterisk.isVisible();
+}
+async getSecondFieldText() {
+  return await this.passwordFieldLabel.textContent();
+}
+
+async isInputFieldCentered() {
+  const box = await this.usernameInput.boundingBox();
+  const viewport = await this.page.viewportSize();
+  return Math.abs((viewport.width / 2) - (box.x + box.width / 2)) < 10; // Checks if centered within 10px tolerance
+}
+
+async isLoginButtonPresent() {
+  return await this.submitButton.isVisible();
+}
+
+async getUserFieldPlaceholderColor() {
+  return await this.usernameFieldLabel.evaluate(el => getComputedStyle(el).color);
+}
+
+async getPasswordFieldPlaceholderColor() {
+  return await this.passwordFieldLabel.evaluate(el => getComputedStyle(el).color);
+}
+
+
 }
 
 export { LoginPage };
